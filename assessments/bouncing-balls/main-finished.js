c0548cbb903a999bff08e0f9e13ad0e8f040e35f
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-03 16:53:17 -0400

    Capture and display count
    - Create a variable that stores a reference to the paragraph.
    - Keep a count of the number of balls on screen in some way.
    - Increment the count and display the updated number of balls each time a ball is added to the scene.
    - Decrement the count and display the updated number of balls each time the evil circle eats a ball (causes it not to exist).

--- assessments/bouncing-balls/main-finished.js
+++ assessments/bouncing-balls/main-finished.js
@@ -186,23 +186,32 @@ let evilBall = new EvilConstructor(
 );
 evilBall.setControls();
 
+
+// capture number of balls displayed
+
+let count = balls.length;
+document.getElementsByTagName('p')[0].innerText = 'Ball count: ' + String(count); 
+
 // define loop that keeps drawing the scene constantly
 
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
 
+  count = 0;
   for(let i = 0; i < balls.length; i++) {
     if (balls[i].exists) {
       balls[i].draw();
       balls[i].update();
       balls[i].collisionDetect();
+      count++;
     }
     evilBall.draw();
     evilBall.checkBounds();
     evilBall.collisionDetect();
   }
-
+  document.getElementsByTagName('p')[0].innerText = 'Ball count: ' + String(count); 
+  
   requestAnimationFrame(loop);
 }
 
