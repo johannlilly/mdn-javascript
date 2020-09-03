bb6e4f4cfa21f83434035be703c7347a128c45fd
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-03 11:54:29 -0400

    add line width property

--- assessments/bouncing-balls/main-finished.js
+++ assessments/bouncing-balls/main-finished.js
@@ -62,6 +62,7 @@ Ball.prototype.draw = function() {
 
 EvilConstructor.prototype.draw = function() {
   ctx.beginPath();
+  ctx.lineWidth = 3;
   ctx.strokeStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.stroke();
