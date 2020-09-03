0c6e0d2efceb48cbcce2efc90d8b25c1f31ea0cb
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-03 12:31:04 -0400

    define collisionDetect method on EvilConstructor
    In the outer if statement, you no longer need to check whether the current ball in the iteration is the same as the ball that is doing the checking — because it is no longer a ball, it is the evil circle! Instead, you need to do a test to see if the ball being checked exists (with which property could you do this with?). If it doesn't exist, it has already been eaten by the evil circle, so there is no need to check it again.

--- assessments/bouncing-balls/main-finished.js
+++ assessments/bouncing-balls/main-finished.js
@@ -127,6 +127,20 @@ Ball.prototype.collisionDetect = function() {
   }
 };
 
+// define evil constructor collisionDetect method
+
+EvilConstructor.prototype.collisionDetect = function() {
+  for(let j = 0; j < balls.length; j++) {
+    const dx = this.x - balls[j].x;
+    const dy = this.y - balls[j].y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < this.size + balls[j].size) {
+      balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+    }
+  }
+};
+
 // define evil constructor setControls method
 
 EvilConstructor.prototype.setControls = function() {
