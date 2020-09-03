75233a90519a4bb9e74e36a1840178b279084eaa
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-03 10:21:10 -0400

    change your existing Ball() constructor so that it becomes a Shape() constructor

--- assessments/bouncing-balls/main-finished.js
+++ assessments/bouncing-balls/main-finished.js
@@ -13,9 +13,9 @@ function random(min, max) {
   return num;
 }
 
-// define Ball constructor
+// define shape constructor
 
-function Ball(x, y, velX, velY, color, size) {
+function Shape(x, y, velX, velY, color, size) {
   this.x = x;
   this.y = y;
   this.velX = velX;
@@ -24,18 +24,18 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-// define ball draw method
+// define shape draw method
 
-Ball.prototype.draw = function() {
+Shape.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
 };
 
-// define ball update method
+// define shape update method
 
-Ball.prototype.update = function() {
+Shape.prototype.update = function() {
   if((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
@@ -56,31 +56,31 @@ Ball.prototype.update = function() {
   this.y += this.velY;
 };
 
-// define ball collision detection
+// define shape collision detection
 
-Ball.prototype.collisionDetect = function() {
-  for(let j = 0; j < balls.length; j++) {
-    if(!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
+Shape.prototype.collisionDetect = function() {
+  for(let j = 0; j < shapes.length; j++) {
+    if(!(this === shapes[j])) {
+      const dx = this.x - shapes[j].x;
+      const dy = this.y - shapes[j].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
 
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+      if (distance < this.size + shapes[j].size) {
+        shapes[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
       }
     }
   }
 };
 
-// define array to store balls and populate it
+// define array to store shapes and populate it
 
-let balls = [];
+let shapes = [];
 
-while(balls.length < 25) {
+while(shapes.length < 25) {
   const size = random(10,20);
-  let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
+  let shape = new Shape(
+    // shape position always drawn at least one Shape width
+    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
     random(0 + size,height - size),
     random(-7,7),
@@ -88,7 +88,7 @@ while(balls.length < 25) {
     'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
     size
   );
-  balls.push(ball);
+  shapes.push(shape);
 }
 
 // define loop that keeps drawing the scene constantly
@@ -97,10 +97,10 @@ function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
 
-  for(let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+  for(let i = 0; i < shapes.length; i++) {
+    shapes[i].draw();
+    shapes[i].update();
+    shapes[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
